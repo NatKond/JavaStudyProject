7407eebce523471b72d1b7517f6d88115eb15067
NatKond <natzubova@gmail.com>
2024-11-24 20:11:11 +0100

    Add Lesson 2

---
+++ src/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+    public int getSum(int a, int b){
+        return a + b;
+    }
+
+    public int getSub(int a, int b){
+        return a - b;
+    }
+
+    public int getMul(int a, int b){
+        return a * b;
+    }
+
+    public int getDev(int a, int b){
+        return a / b;
+    }
+
+}
--- src/Study.java
+++ src/Study.java
@@ -1,6 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Study {
+    private static void sayHello(String name) {
+        System.out.println("Hello " + name);
+    }
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        String str = "I study Basic Java!";
+        sayHello(str);
+        System.out.println(str.charAt(str.length() - 1));
+        System.out.print("str contains Java: ");
+        System.out.println(str.contains("Java"));
+        System.out.println(str.replace('a', 'o'));
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+        System.out.println(str.substring(str.length()-5, str.length()));
+        System.out.println();
+
+        Calculator calculator = new Calculator();
+        int a = 15;
+        int b = 5;
+        System.out.println("a = " + a + ", b = " + b );
+        System.out.println("a + b = " + calculator.getSum(a,b));
+        System.out.println("a - b =  " + calculator.getSub(a,b));
+        System.out.println("a * b =  " + calculator.getMul(a,b));
+        System.out.println("a / b =  " + calculator.getDev(a,b));
     }
 }
