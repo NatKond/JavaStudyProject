160b9828949c6b2e531323992477734f09084c7d
NatKond <natzubova@gmail.com>
2024-11-26 11:12:38 +0100

    Add Lesson6

---
+++ src/Dog.java
@@ -0,0 +1,39 @@
+public class Dog {
+
+    private int age;
+    private String name;
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void addName(String surname){
+        this.name = name + "-" + surname;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public int getSum(int a, int b){
+        int sum = a + b;
+        System.out.println(a + " + " + b + " = " + sum);
+        return sum;
+    }
+}
--- src/Study.java
+++ src/Study.java
@@ -1,6 +1,14 @@
 
 public class Study {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        Dog dog = new Dog();
+        dog.setAge(5);
+        dog.setName("Bobik");
+        dog.addName("Tusik");
+        System.out.println(dog.toString());
+        int a = 10;
+        int b = 2;
+        int sum = dog.getSum(10, 15);
+        System.out.print("a = " + a + ", b = " + b + ", sum = " + sum);
     }
 }
